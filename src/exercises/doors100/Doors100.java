7bba5362fb555b0c29a5c303e781606f5ce3b507
kpkurian <kp.kurian@gmail.com>
2013-02-19 15:36:20 -0500

    first commit

---
+++ src/exercises/doors100/Doors100.java
@@ -0,0 +1,11 @@
+package exercises.doors100;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kkurian
+ * Date: 2/19/13
+ * Time: 3:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Doors100 {
+}
